635255ff259789cc3de166e761ea83ca9766cd7e
JanekMagda <jan_walczak@wp.pl>
2021-01-07 12:17:38 +0100

    Homework_1.py

--- Copy.py
+++ Copy.py
@@ -1,4 +1,3 @@
-
 import csv
 
 GOOG = "GOOG.csv"
@@ -10,32 +9,47 @@ IBM_2 = "IBM_2.csv"
 MSFT = "MSFT.csv"
 MSFT_2 = "MSFT_2.csv"
 
+
 with open(GOOG, 'r', newline='') as f, open(GOOG_2, 'w', newline='') as data:
-    writer = csv.writer(data, quoting=csv.QUOTE_ALL)
-    writer.writerows(line.split() for line in f)
+     writer = csv.writer(data, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=',')
+     writer.writerows(line.split() for line in f)
 
-    import pandas as pd
-
-    new_val = pd.read_csv("GOOG_2.csv")
-    new_val['Change'] = new_val['Close'] - new_val['Open']
-    new_val.to_csv('GOOG_2.csv', index=False)
+try:
+     import pandas as pd
+     new_val = pd.read_csv('GOOG_2.csv', sep=',', usecols=['Date', 'Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
+     new_val['Change'] = (new_val['Close'] - new_val['Open']) / new_val['Open']
+     new_val.to_csv('Polaczone_2.csv', index=False)
+except:
+     print("Something went wrong when writing to the file")
+finally:
+    f.close()
 
     with open(IBM, 'r', newline='') as f, open(IBM_2, 'w', newline='') as data:
-        writer = csv.writer(data, quoting=csv.QUOTE_ALL)
+        writer = csv.writer(data, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=',')
         writer.writerows(line.split() for line in f)
 
-        import pandas as pd
+try:
+    import pandas as pd
 
-        new_val = pd.read_csv("IBM_2.csv")
-        new_val['Change'] = new_val['Close'] - new_val['Open']
-        new_val.to_csv('IBM_2.csv', index=False)
+    new_val = pd.read_csv('IBM_2.csv', sep=',', usecols=['Date', 'Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
+    new_val['Change'] = (new_val['Close'] - new_val['Open']) / new_val['Open']
+    new_val.to_csv('Polaczone_2_IBM.csv', index=False)
+except:
+    print("Something went wrong when writing to the file")
+finally:
+    f.close()
 
-        with open(MSFT, 'r', newline='') as f, open(MSFT_2, 'w', newline='') as data:
-            writer = csv.writer(data, quoting=csv.QUOTE_ALL)
-            writer.writerows(line.split() for line in f)
+    with open(MSFT, 'r', newline='') as f, open(MSFT_2, 'w', newline='') as data:
+        writer = csv.writer(data, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=',')
+        writer.writerows(line.split() for line in f)
 
-            import pandas as pd
+try:
+    import pandas as pd
 
-            new_val = pd.read_csv("MSFT_2.csv")
-            new_val['Change'] = new_val['Close'] - new_val['Open']
-            new_val.to_csv('MSFT_2.csv', index=False)
+    new_val = pd.read_csv('MSFT_2.csv', sep=',', usecols=['Date', 'Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
+    new_val['Change'] = (new_val['Close'] - new_val['Open']) / new_val['Open']
+    new_val.to_csv('Polaczone_2_MSFT.csv', index=False)
+except:
+    print("Something went wrong when writing to the file")
+finally:
+    f.close()
